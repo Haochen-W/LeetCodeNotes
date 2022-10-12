64995b56ee8bf1166d17110d9c6ef95ffce8cd62
Haochen-W <jason.haochenwu@qq.com>
2022-10-11 22:34:08 -0400

    add linkedlist

--- algo.cpp
+++ algo.cpp
@@ -13,6 +13,30 @@
 
 using namespace std;
 
+namespace LinkedList
+{
+    struct ListNode
+    {
+        int val;
+        ListNode *next;
+        ListNode() : val(0), next(nullptr) {}
+        ListNode(int x) : val(x), next(nullptr) {}
+        ListNode(int x, ListNode *next) : val(x), next(next) {}
+    };
+    ListNode *middleNode(ListNode *head)
+    {
+        if (!head)
+            return nullptr;
+        ListNode *slow = head, *fast = head;
+        while (fast && fast->next)
+        {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        return slow;
+    }
+} // namespace LinkedList
+
 namespace unionFind
 {
     struct DisjointSetUnionBySize
